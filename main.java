90fef06d7b96a836620d2c86da4b93acc0d914e6
Trungnd951206 <ductrung612@gmail.com>
2023-12-28 14:29:10 +0700

    add number print method

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
 public class main {
   public static void main(String[] args) {
+    // This is a comment and will be ignored by the compiler
+    /*This is a comment 
+    in mutil-line */
     System.out.println("Hello World");
+    System.out.println("I am leaning Java");
+    System.out.println("Is it fun? ");
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
   }
 }
