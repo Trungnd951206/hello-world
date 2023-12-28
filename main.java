47f91a8d965cf5bce87a432b7a3b75b4906c63d7
Trungnd951206 <ductrung612@gmail.com>
2023-12-28 14:35:37 +0700

    updated main.java with emergency fix

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
   public static void main(String[] args) {
     System.out.println("Hello World");
-  }
+    String name = "John";
+    System.out.println(name);
+    int myNum = 15;
+    System.out.println(myNum);
+}
 }
