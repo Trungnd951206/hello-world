41c374ba03dc945035db4cc42a93f3fd3c15a273
Trungnd951206 <ductrung612@gmail.com>
2023-12-28 15:39:42 +0700

    update method

--- main.java
+++ main.java
@@ -4,5 +4,14 @@ public class main {
     /*This is a comment 
     in mutil-line */
     System.out.println("Hello World");
+    int myNum = 5;
+    int minutesPerHour = 60;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String firstName = "John ", lastName = "Doe", fullName = firstName + lastName;
+        System.out.println(fullName);
+    int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
   }
 }
